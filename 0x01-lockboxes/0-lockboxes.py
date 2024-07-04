08cb6ff30b4b70f8e9f583bf0bae77c9ef5377bf
loxiasblack <iliaschokairi@gmail.com>
2024-07-04 11:34:49 +0100

    push the solution

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Create a function that does returun if boxes can
+be unlcoked or not
+"""
+
+
+def canUnlockAll(boxes):
+    """ Method that determines if all boxes can be opened """
+
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+
+    return True
