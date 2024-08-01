f2bcbffa76932913b098a72049254be63525dfed
loxiasblack <iliaschokairi@gmail.com>
2024-08-01 15:30:23 +0100

    hey

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,60 +1,55 @@
-#!/usr/bin/python3
-"""
-"""
 
+#!/usr/bin/python3
+"""Solving N Queens with Backtracing"""
 import sys
 
 
-def n_queens(n):
-    """ N queens solution """
-    queens, res = [], []
-    cols, positive_diag, negative_diag = set(), set(), set()
-
-    def backtrack(row, n, queens):
-        """ Backtracking function """
-        if row == n:
-            res.append(queens[:])
-            return
-        for col in range(n):
-            if (col in cols or row + col in positive_diag or
-                    row - col in negative_diag):
-                continue
-            cols.add(col)
-            positive_diag.add(row + col)
-            negative_diag.add(row - col)
-            queens.append([row, col])
-            backtrack(row + 1, n, queens)
-
-            cols.remove(col)
-            positive_diag.remove(row + col)
-            negative_diag.remove(row - col)
-            queens.pop()
-    backtrack(0, n, queens)
-    return res
-
-
-def check_args(n):
-    """ Check if n is a valid argument """
-    if not n.isdigit():
-        print("N must be a number")
-        exit(1)
-    if int(n) < 4:
-        print("N must be at least 4")
-        exit(1)
+def nqueens(n, y, board):
+    """
+    Method: nqueens - place n queens
+            on an n by n board so that
+            no queens are attacking any
+            others.
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
+            placement, in list of lists form
+    """
+    for x in range(n):
+        hold = 0
+        for q in board:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
 
 
 def main():
-    """ Main function """
-    args = sys.argv
-    if len(args) != 2:
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-    n = args[1]
-    check_args(n)
-    solutions = n_queens(int(n))
-    for solution in solutions:
-        print(solution)
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
+    nqueens(n, 0, [])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
