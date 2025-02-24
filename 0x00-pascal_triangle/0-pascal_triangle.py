8a404ffa793460e046f493b54e789c4dcead7e99
loxiasblack <iliaschokairi@gmail.com>
2025-02-24 22:34:50 +0000

    push

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-
 """function"""
+
+
 def pascal_triangle(n):
-    """"""
+    """
+    define the function
+    """
     pascal = []
     for i in range(n+1):
-        row = [pascal[i-1][y-1] + pascal[i-1][y] if (y < i-1 and y!=0) else 1  for y in range(i)]
+        row = [pascal[i - 1][y - 1] + pascal[i - 1][y]
+               if (y < i - 1 and y != 0) else 1 for y in range(i)]
         pascal.append(row)
     pascal.pop(0)
     return pascal
